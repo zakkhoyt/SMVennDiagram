4da271e76be6eb3592c2aa64579cfe1d4f6cc924
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-03-17 18:37:45 -0700

    Initial Commit

---
+++ VennDiagram/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  VennDiagram
+//
+//  Created by Zakk Hoyt on 3/17/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VennDiagram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VennDiagram
+//
+//  Created by Zakk Hoyt on 3/17/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
